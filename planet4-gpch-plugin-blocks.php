4e04a61cc997c2eaf97f499205bacfcdf9b36998
Stefan Dürrenberger <stduerre@greenpeace.org>
2019-04-17 09:36:37 +0200

    Basic plugin files

---
+++ planet4-gpch-plugin-blocks.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Planet4 GPCH Plugin Blocks
+ * Plugin URI: https://github.com/greenpeace/planet4-gpch-plugin-blocks
+ * Description: Provides GPCH specific content blocks for Planet4
+ * Version: 0.0.1
+ * License: MIT
+ * Text Domain: planet4-gpch-blocks
+ */
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
