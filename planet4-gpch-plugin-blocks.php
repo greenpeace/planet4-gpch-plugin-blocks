4d707ff86dbaa243c94b5b879afda10fb4b73728
Stefan Dürrenberger <stduerre@greenpeace.org>
2019-05-03 17:32:01 +0200

    Changed plugin description

--- planet4-gpch-plugin-blocks.php
+++ planet4-gpch-plugin-blocks.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Planet4 GPCH Plugin Blocks
  * Plugin URI: https://github.com/greenpeace/planet4-gpch-plugin-blocks
- * Description: Provides GPCH specific content blocks for Planet4
+ * Description: Provides Planet4 content blocks specific to Greenpeace Switzerland
  * Version: 0.0.2
  * License: MIT
  * Text Domain: planet4-gpch-blocks
