e43f73ca6a4b5eab7315f6166be1cf5418f26138
Stefan Dürrenberger <stduerre@greenpeace.org>
2019-05-03 18:57:38 +0200

    Removed Twig again
    Timber already uses Twig, standalone install causes issues.

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,9 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Twig\\' => array($vendorDir . '/twig/twig/src'),
-    'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
-    'Symfony\\Polyfill\\Ctype\\' => array($vendorDir . '/symfony/polyfill-ctype'),
     'Greenpeace\\Planet4GPCHBlocks\\Blocks\\' => array($baseDir . '/includes/blocks'),
     'Greenpeace\\Planet4GPCHBlocks\\' => array($baseDir . '/includes'),
 );
