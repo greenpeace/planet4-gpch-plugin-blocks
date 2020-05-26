d6e33878cb05d941bb869fd9207078362b042150
Stefan Dürrenberger <stduerre@greenpeace.org>
2020-05-26 17:56:44 +0200

    359: Import base dictionary on activation of plugin

--- planet4-gpch-plugin-blocks.php
+++ planet4-gpch-plugin-blocks.php
@@ -33,6 +33,7 @@ $p4_gpch_plugin_blocks = Greenpeace\Planet4GPCHBlocks\Planet4_GPCH_Plugin_Blocks
 
 // Activation hooks
 register_activation_hook( __FILE__, 'gpch_plugin_blocks_db_install' );
+register_activation_hook( __FILE__, 'gpch_plugin_blocks_db_insert_data' );
 
 // Other Hooks
 add_action( 'admin_menu', 'gpch_cloud_block_admin_menu' );
@@ -68,4 +69,44 @@ function gpch_plugin_blocks_db_install() {
 	dbDelta( $sql );
 
 	add_option( 'gpchdict_db_version', $gpchdict_db_version );
+}
+
+/**
+ * Installs the base dictionary used for the cloud.
+ */
+function gpch_plugin_blocks_db_insert_data() {
+	global $wpdb;
+
+	$table_name = $wpdb->prefix . "gpch_wordcloud_dictionary";
+
+	// Only run if the table is empty
+	$dictionary_count = $wpdb->get_var( "SELECT COUNT(*) FROM $table_name" );
+	var_dump($dictionary_count);
+
+	if ( $dictionary_count == 0 ) {
+		$file = P4_GPCH_PLUGIN_BLOCKS_BASE_PATH . "/data/wordcloud-dictionary-de.csv";
+
+		$file_handler = fopen( $file, "r" );
+		$j = 0;
+
+		// The file might be too big to import all at once. Let's read 1000 lines at a time.
+		while ( ! feof( $file_handler ) ) {
+			$format = '(\'%s\', \'%s\', \'%s\', \'%s\', \'%s\')';
+			$sql    = 'INSERT INTO ' . $table_name . ' (language, confirmed, blacklisted, word, type) VALUES ';
+
+			// Combine 1000 lines, because using wpdb->insert() for each line separately takes too much time
+			for ( $i = 0; $i < 1000 && ! feof( $file_handler ); $i ++ ) {
+				$entry = fgetcsv( $file_handler );
+
+				$sql .= sprintf( $format, addslashes($entry[0]), addslashes($entry[1]), addslashes($entry[2]), addslashes($entry[3]), addslashes($entry[4]) ) . ", ";
+			}
+
+			// cleanup trailing comma
+			$sql = rtrim( $sql, ", " ) . ';';
+
+			$wpdb->query( $sql );
+		}
+
+		fclose( $file_handler );
+	}
 }
