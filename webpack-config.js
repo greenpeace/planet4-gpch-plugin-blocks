6781fd7422beb482329a7b6c15fc7b92e60c7182
Stefan D <stefan.duerrenberger@greenpeace.org>
2021-03-03 15:40:22 +0100

    Add Babel config

--- webpack-config.js
+++ webpack-config.js
@@ -1,4 +1,8 @@
-const defaultConfig = require('./node_modules/@wordpress/scripts/config/webpack.config');    // Require default Webpack config
+const defaultConfig = require(
+  './node_modules/@wordpress/scripts/config/webpack.config' )    // Require
+                                                                  // default
+                                                                  // Webpack
+                                                                  // config
 const path = require( 'path' )
 
 const config = {}
@@ -19,11 +23,10 @@ var backendConfig = {
 	},
 }
 
-
-const frontendConfig = Object.assign({}, config, {
+const frontendConfig = Object.assign( {}, config, {
 	entry: {
 		// Frontend JS for blocks
-		bsBingo: './src/blocks/BSBingo/BSBingoBlock-frontend.js'
+		bsBingo: './src/blocks/BSBingo/BSBingoBlock-frontend.js',
 	},
 	
 	output: {
@@ -32,8 +35,22 @@ const frontendConfig = Object.assign({}, config, {
 		// Specify the path to the JS files.
 		path: path.resolve( __dirname, 'build/js/blocks' ),
 	},
-});
-
+	
+	// Setup a loader to transpile down the latest and great JavaScript so
+	// older browsers can understand it.
+	module: {
+		rules: [
+			{
+				// Look for any .js files.
+				test: /\.js$/,
+				// Exclude the node_modules folder.
+				exclude: /node_modules/,
+				// Use babel loader to transpile the JS files.
+				loader: 'babel-loader',
+			},
+		],
+	},
+} )
 
 // Export the config object.
 module.exports = [backendConfig, frontendConfig]
