27290a61e4903e8e74e7ca6b235e985b3922237d
Stefan D <46678842+stduerre@users.noreply.github.com>
2023-01-04 14:09:12 +0100

    Feature/623 update dependencies (#94)
    * 623: Update dependencies
    
    * 623: Run formatter
    
    * 623: Update readme
    
    * 623: Update Browserlist

--- assets/js/blocks.js
+++ assets/js/blocks.js
@@ -1,31 +1,25 @@
 jQuery( document ).ready( function() {
 	// Action Divider Block
-	( function( jQuery ) {
-		var yesButton = jQuery(
-		  '.action-divider .action-divider-question-button-yes' )
-		var noButton = jQuery(
-		  '.action-divider .action-divider-question-button-no' )
-		
-		jQuery( yesButton ).click( function() {
-			var actionDividerBlock = jQuery( this ).
-			  closest( '.action-divider' )
-			
-			actionDividerBlock.find( '.action-divider-question-wrapper' ).
-			  slideUp()
-			actionDividerBlock.find( '.action-divider-step2-yes' ).slideDown()
-			
-			return false
-		} )
-		
-		jQuery( noButton ).click( function() {
-			var actionDividerBlock = jQuery( this ).
-			  closest( '.action-divider' )
-			
-			actionDividerBlock.find( '.action-divider-question-wrapper' ).
-			  slideUp()
-			actionDividerBlock.find( '.action-divider-step2-no' ).slideDown()
-			
-			return false
-		} )
-	} )( jQuery )
-} )
+	(function (jQuery) {
+		var yesButton = jQuery('.action-divider .action-divider-question-button-yes');
+		var noButton = jQuery('.action-divider .action-divider-question-button-no');
+
+		jQuery(yesButton).click(function () {
+			var actionDividerBlock = jQuery(this).closest('.action-divider');
+
+			actionDividerBlock.find('.action-divider-question-wrapper').slideUp();
+			actionDividerBlock.find('.action-divider-step2-yes').slideDown();
+
+			return false;
+		});
+
+		jQuery(noButton).click(function () {
+			var actionDividerBlock = jQuery(this).closest('.action-divider');
+
+			actionDividerBlock.find('.action-divider-question-wrapper').slideUp();
+			actionDividerBlock.find('.action-divider-step2-no').slideDown();
+
+			return false;
+		});
+	})(jQuery);
+});
