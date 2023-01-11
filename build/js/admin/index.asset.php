a3438cad3d1b50477d421ac8fa71cc6373130eb9
Stefan D <stefan.duerrenberger@greenpeace.org>
2023-01-11 08:32:42 +0100

    Build blocks

--- build/js/admin/index.asset.php
+++ build/js/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'e5d887d62e9c4fc8115313406b2f5811');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'c6068197af86af7d2d2f');
