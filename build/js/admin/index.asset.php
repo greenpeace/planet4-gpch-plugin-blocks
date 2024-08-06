e9b2c74f67f20f5331a17e2ec1cb28da4a07ac8e
Stefan D <stefan.duerrenberger@greenpeace.org>
2024-08-06 10:02:24 +0200

    Update version number & build

--- build/js/admin/index.asset.php
+++ build/js/admin/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '9a936e91215d58eda91a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '2cd9e0994a87733738bc');
